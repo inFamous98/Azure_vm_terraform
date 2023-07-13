fe5cc3e40353a5e37d1f0baedf49c5986d64e1ae
inFamous98 <66897214+inFamous98@users.noreply.github.com>
2023-07-13 17:41:52 +0530

    Add files via upload

---
+++ virtual_machine/main.tf
@@ -0,0 +1,43 @@
+resource "azurerm_storage_account" "example" {
+  count                 = length(var.vm_configurations) > 0 ? 1 : 0
+  name                  = var.storage_account_name
+  resource_group_name   = var.resource_group_name
+  location              = var.location
+  account_tier          = var.storage_account_tier
+  account_replication_type   = var.storage_account_replication
+}
+
+resource "azurerm_virtual_machine" "example" {
+  count                 = length(var.vm_configurations) > 0 ? sum([for config in var.vm_configurations : config.count]) : 0
+  name                  = "${var.vm_configurations[count.index % length(var.vm_configurations)].name}-${count.index}"
+  location              = var.vm_configurations[count.index % length(var.vm_configurations)].location
+  resource_group_name   = var.vm_configurations[count.index % length(var.vm_configurations)].resource_group_name
+  network_interface_ids = [var.vm_configurations[count.index % length(var.vm_configurations)].network_interface_id]
+  vm_size               = var.vm_configurations[count.index % length(var.vm_configurations)].vm_size
+
+  storage_os_disk {
+    name              = "${var.vm_configurations[count.index % length(var.vm_configurations)].name}-${count.index}-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+    disk_size_gb      = var.vm_configurations[count.index % length(var.vm_configurations)].os_disk_size
+  }
+
+  storage_image_reference {
+    publisher = var.vm_configurations[count.index % length(var.vm_configurations)].os_publisher
+    offer     = var.vm_configurations[count.index % length(var.vm_configurations)].os_offer
+    sku       = var.vm_configurations[count.index % length(var.vm_configurations)].os_sku
+    version   = var.vm_configurations[count.index % length(var.vm_configurations)].os_version
+  }
+
+  dynamic "storage_data_disk" {
+    for_each = var.vm_configurations[count.index % length(var.vm_configurations)].extra_disk_sizes != null ? var.vm_configurations[count.index % length(var.vm_configurations)].extra_disk_sizes : []
+    content {
+      name              = "${var.vm_configurations[count.index % length(var.vm_configurations)].name}-${count.index}-extradisk-${storage_data_disk.key}"
+      create_option     = "Empty"
+      managed_disk_type = "Standard_LRS"
+      disk_size_gb      = storage_data_disk.value
+      lun               = storage_data_disk.key + 1
+    }
+  }
+}
